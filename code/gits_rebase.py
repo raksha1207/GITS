f9be9da92f02d868d3eb875964affeea78ac6a3d
raksha1207 <mssreeraksha@gmail.com>
2020-09-21 23:51:03 -0400

    new_change_

--- code/gits_rebase.py
+++ code/gits_rebase.py
@@ -11,13 +11,15 @@ def gits_rebase(args):
         if inp.lower() == "yes" or inp.lower() == "y":
             process1 = subprocess.Popen(['git', 'rebase', 'master'], stdout=PIPE, stderr=PIPE)
             stdout, stderr = process1.communicate()
-            # print(stdout)
+            print(stdout)
+            print(stderr)
         else:
             inp2 = input("Enter the name of the branch you want to rebase")
             # print(inp2)
             process2 = subprocess.Popen(['git', 'checkout', inp2, 'git', 'rebase', 'master'], stdout=PIPE, stderr=PIPE)
             stdout, stderr = process2.communicate()
-            # print(stdout)
+            print(stdout)
+            print(stderr)
     except Exception as e:
         print("ERROR: gits reset command caught an exception")
         print("ERROR: {}".format(str(e)))
